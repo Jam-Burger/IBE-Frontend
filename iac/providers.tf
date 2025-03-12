a12fd3dae69d57372d618771d771635a51f2cf87
Jay Malaviya <jay.malaviya@kickdrumtech.com>
2025-03-13 01:48:37 +0530

    feature/workflows: removed backend.config generation script

--- iac/providers.tf
+++ iac/providers.tf
@@ -1,5 +1,11 @@
 terraform {
-  backend "s3" {}
+  backend "s3" {
+    bucket         = "${var.project_name}-${var.team_name}-terraform-frontend-state"
+    key            = "terraform.tfstate"
+    region         = var.aws_region
+    dynamodb_table = "${var.project_name}-${var.team_name}-terraform-frontend-locks"
+    encrypt        = true
+  }
 
   required_providers {
     aws = {
