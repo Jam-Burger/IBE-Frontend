f1f4e5aaae0c8407ef8323d823d6c6d446e83d78
Jay Malaviya <jay.malaviya@kickdrumtech.com>
2025-03-15 18:57:46 +0530

    feature/workflows: renamed terraform output

--- iac/outputs.tf
+++ iac/outputs.tf
@@ -6,6 +6,6 @@ output "cloudfront_distribution_id" {
   value = module.s3_cloudfront.cloudfront_distribution_id
 }
 
-output "frontend_bucket_name" {
+output "s3_bucket_name" {
   value = module.s3_cloudfront.bucket_name
 }
